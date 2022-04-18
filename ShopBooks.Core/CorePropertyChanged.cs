30dee572944a5aab06da94523a739cbb6ce08f9b
join-devOps <alexander.lebedev2959@mail.ru>
2022-04-18 10:48:25 +0300

    Добавлен

---
+++ ShopBooks.Core/CorePropertyChanged.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopBooks.Core
+{
+    public class CorePropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
